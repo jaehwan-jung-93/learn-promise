39bf3c9b60c62f438c6935d2e67304bbb6f4098e
jaehwan_jung <jungjh0224@gmail.com>
2020-11-28 15:20:37 +0900

    add img

--- src/index.js
+++ src/index.js
@@ -1,32 +1,56 @@
-const spinner = document.querySelector('img');
+/* Spinner Example Code */
 
-let rotateCount = 0;
-let startTime = null;
-let rAF;
+// const spinner = document.querySelector('img');
 
-function draw(timestamp) {
-    if (!startTime) {
-        startTime = timestamp;
+// let rotateCount = 0;
+// let startTime = null;
+// let rAF;
+
+// function draw(timestamp) {
+//     if (!startTime) {
+//         startTime = timestamp;
+//     }
+
+//     rotateCount = (timestamp - startTime) / 3;
+
+//     if (rotateCount > 359) {
+//         rotateCount %= 360;
+//     }
+
+//     spinner.style.transformOrigin = 'center';
+//     spinner.style.transform = `rotate(${rotateCount}deg)`;
+
+//     rAF = requestAnimationFrame(draw);
+// }
+
+// draw();
+
+// const pageBody = document.querySelector('body');
+
+// function endAnim() {
+//     cancelAnimationFrame(rAF);
+// }
+
+// pageBody.addEventListener('click', endAnim);
+
+/* ************************************************** */
+
+let promise = fetch('img1.png');
+
+promise.then(result => {
+    if(!result.ok) {
+        throw new Error(`HTTP error! status: ${result.status}`);
     }
-
-    rotateCount = (timestamp - startTime) / 3;
-
-    if (rotateCount > 359) {
-        rotateCount %= 360;
+    else {
+        return result.blob();
     }
-
-    spinner.style.transformOrigin = 'center';
-    spinner.style.transform = `rotate(${rotateCount}deg)`;
-
-    rAF = requestAnimationFrame(draw);
-}
-
-draw();
-
-const pageBody = document.querySelector('body');
-
-function endAnim() {
-    cancelAnimationFrame(rAF);
-}
-
-pageBody.addEventListener('click', endAnim);
+})
+.then(myBlob => {
+    let objectURL = URL.createObjectURL(myBlob);
+    let image = document.createElementNS('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+})
+.catch(error => {
+    console.log('There has been a problem with your fetch operation: ' + error.message);
+});
