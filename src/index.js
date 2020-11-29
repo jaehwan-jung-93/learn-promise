b145d6dd5839aa121efbe082b88ac0aab9492a7a
jaehwan_jung <jungjh0224@gmail.com>
2020-11-29 19:53:04 +0900

    fixed tex img url

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ function fetchImg(imgPath) {
     })
 }
 
-fetchImg('https://www.pubg.com/wp-content/uploads/2020/07/banner.png').then(blobdata => {
+fetchImg('/src/img1.png').then(blobdata => {
     console.log(blobdata);
 }).catch (error => {
     console.log('There has been a problem with your fetch operation: ' + error.message);
